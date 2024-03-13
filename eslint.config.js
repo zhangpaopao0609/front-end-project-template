0f53fc62539c5e9e7268f017aac8a12bc37cdb29
zhangpaopao <zhangpaopao0609@gmail.com>
2024-03-13 15:39:48 +0800

    feat: style/brace-style

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,10 @@ import antfu from '@antfu/eslint-config';
 export default antfu({
   stylistic: {
     semi: true,
+    // https://eslint.style/rules
+    overrides: {
+      'style/brace-style': ['error', '1tbs'],
+    },
   },
   ignores: ['tsconfig.json'],
 });
