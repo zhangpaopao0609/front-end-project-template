8888a6fc691c58899a46db77bd24a9c4941a6117
zhangpaopao <zhangpaopao0609@gmail.com>
2024-03-13 16:11:05 +0800

    feat: curly

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,12 @@ export default antfu({
     // https://eslint.style/rules
     overrides: {
       'style/brace-style': ['error', '1tbs'],
+      // 'style/nonblock-statement-body-position': ['off', 'below'],
     },
   },
   ignores: ['tsconfig.json'],
+  rules: {
+    // https://eslint.org/docs/latest/rules/curly
+    curly: ['error', 'all'],
+  },
 });
